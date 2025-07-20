2d25b670248f667da3b48e90980f4507ff994a69
Sanvaad <sanvaad003@gmail.com>
2025-07-20 14:46:18 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# The React Pizza
+
+Things i learnt making this app
+
+- New way of implementing routes
+- Redux state management
+- Settig up and Interacting with an api
---
+++ src/features/user/userSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  username: '',
+  status: 'idle',
+  position: {},
+  address: '',
+  error: '',
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    updateName(state, action) {
+      state.username = action.payload;
+    },
+  },
+});
+
+export const { updateName } = userSlice.actions;
+
+export default userSlice.reducer;
